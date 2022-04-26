3c02e605ea2588f1e5c37e2021ba10a2008eeb9c
leonardogarreto <leonardogarreto@gmail.com>
2022-04-26 15:19:35 -0300

    feat: algumas libs adicionadas (styled-components, aiq-design-system, axios) e adicionado ThemeProvider do aiq-design-system.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "aiq-design-system";
+
+function App() {
+  return (
+    <ThemeProvider>
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  );
+}
+
+export default App;
